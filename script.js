d433a4af322599c453824f330bc28dda20a00e9b
Élaff Ramos <elaff.dev@gmail.com>
2025-02-27 13:06:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-const botao = document.querySelector('.botao')
-
 function pagina() {
     window.location.href = 'form.html'
 }
 
-function toggleMenu() {
-    let menu = document.querySelector(".menu");
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const menu = document.querySelector(".menu");
 
-    // Alterna entre 'block' e 'none'
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "block";
-    }
-}
+    menuToggle.addEventListener("click", function () {
+        menu.classList.toggle("active"); // Alterna entre mostrar e esconder o menu
+    });
+});
